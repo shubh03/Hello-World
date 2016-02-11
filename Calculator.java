dc924adfdbb4762c1b0517922cd6f851fa3c94c9
shubh03 <shubhraagrahari11@gmail.com>
2016-02-11 16:16:24 +0530

    Create Calculator.java
    Java code to add two integer value

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+	int add(int num1,int num2){
+		return num1+num2;
+	}
+	public static void main(String[] args) {
+		Calculator obj =new Calculator();
+		System.out.println(obj.add(10, 20));
+
+	}
+
+}
